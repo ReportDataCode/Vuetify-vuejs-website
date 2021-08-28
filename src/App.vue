e7936708cd51996b0946a2ee4bce81122a38f672
tusharhamed <tushar@nogorsolutions.com>
2021-08-29 00:35:32 +0600

    full project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+    <!--Hero section-->
+      <HeroComponent />
+      <!-- About us  -->
+      <Aboutus />
+      <!-- Portfolio  -->
+      <Portfolio />
+      <!-- Project  -->
+      <Project />
+      <!-- Gallery  -->
+      <Gallery />
+      <!-- Contact  -->
+      <Contactus />
+      <!-- Footer  -->
+      <Footer />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Header from "./components/Header";
+import HeroComponent from "./components/HeroComponent";
+import Aboutus from "./components/Aboutus";
+import Portfolio from "./components/Portfolio";
+import Project from "./components/Project";
+import Gallery from "./components/Gallery";
+import Contactus from "./components/Contactus";
+import Footer from "./components/Footer";
+export default {
+  name: 'App',
+  components: {Footer, Contactus, Gallery, Project, Portfolio, Aboutus, HeroComponent, Header},
+  data(){
+    return{
+      menus:[
+        {title:'Home',      route:'home'},
+        {title:'About us',  route:'about-us'},
+        {title:'Protfolio', route:'protfolio'},
+        {title:'Project',   route:'project'},
+        {title:'Gallery',   route:'galley'},
+        {title:'Contact',   route:'contact'},
+      ]
+    }
+  }
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-footer padless>
+        <v-col lg="12">
+            <v-row justify="center">
+                <v-col lg="4" class="text-center">
+                    <v-toolbar-title>Web Burden</v-toolbar-title>
+                    <v-card-text>
+                        <v-btn
+                                v-for="icon in icons"
+                                :key="icon"
+                                class="mx-4 black--text"
+                                icon
+                        >
+                            <v-icon size="24px">
+                                {{ icon }}
+                            </v-icon>
+                        </v-btn>
+                    </v-card-text>
+                    <v-subheader>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem blanditiis
+                        deserunt eius impedit nihil obcaecati perspiciatis quo sunt voluptate.
+                    </v-subheader>
+                    <v-form class="mt-4">
+                        <v-text-field label="Email" outlined dense />
+                        <v-btn rounded color="success">Subscribe us</v-btn>
+                    </v-form>
+                </v-col>
+
+            </v-row>
+            <div class="mt-10">
+                <v-divider></v-divider>
+                <v-subheader>
+                    © Copyright Web Burden. All Rights Reserved {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+                    <v-spacer />
+                    <strong>Designed by Tushar Ahmed</strong>
+                </v-subheader>
+            </div>
+        </v-col>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer",
+        data() {
+            return {
+                icons: [
+                    'mdi-facebook',
+                    'mdi-twitter',
+                    'mdi-linkedin',
+                    'mdi-instagram',
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
